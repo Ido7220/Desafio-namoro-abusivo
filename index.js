9350db2bf9fc7e02de3df1fa1e94c6e3d4aebf81
Renan Ido <iddo.renan@gmail.com>
2025-03-29 00:52:24 -0300

    uptade2

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-const validandoClickNao = document.getElementById('nao')
-const validandoClickSim = document.getElementById('sim')
+const validandoClickNao = document.getElementById('nao');
+const validandoClickSim = document.getElementById('sim');
 
-
-    validandoClickNao.addEventListener('click', () => {
-
-        const randomX = Math.floor(Math.random() * (window.innerWidth - validandoClickNao.offsetWidth));
-        const randomY = Math.floor(Math.random() * (window.innerHeight - validandoClickNao.offsetHeight));
-        
-
-        validandoClickNao.style.position = 'absolute';
-        validandoClickNao.style.left = randomX + 'px';
-        validandoClickNao.style.top = randomY + 'px';
-
-    });
-
-
+validandoClickNao.addEventListener('mouseover', () => {
+    const randomX = Math.floor(Math.random() * (window.innerWidth - validandoClickNao.offsetWidth));
+    const randomY = Math.floor(Math.random() * (window.innerHeight - validandoClickNao.offsetHeight));
+    validandoClickNao.style.position = 'absolute';
+    validandoClickNao.style.left = randomX + 'px';
+    validandoClickNao.style.top = randomY + 'px';
+});
 
 if (validandoClickSim) {
     validandoClickSim.addEventListener('click', () => {
-        alert('Adicione meu número 1234-5678')
+        alert('Adicione meu número: 1234-5678 ❤️');
     });
 }
